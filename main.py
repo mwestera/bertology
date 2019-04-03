d164b5c3ad50944b0fe7a7c87aed305d635a7f41
matthijswestera <matthijs.westera@gmail.com>
2019-04-03 15:27:11 +0200

    implement difference plots

--- main.py
+++ main.py
@@ -77,7 +77,7 @@ bert_version = 'bert-base-cased'    # TODO Why no case?
 
 TRANSPOSE = True    # True to plot as "rows influenced by cols" (otherwise: rows influencing cols).
 
-METHOD = "pat" # "pat" or "mat"     # TODO Allow cumulative MAT too
+METHOD = "PAT" # "PAT" or "MAT"     # TODO Allow cumulative MAT too... CAT?
 GROUPED = True
 LAYER_NORM = True
     # What about normalizing per layer, instead of per head? Does that make any sense? Yes, a bit.
@@ -195,7 +195,7 @@ for _, each_item in items.iterrows():
 
     attention = attention.squeeze()
 
-    weights_per_layer = (compute_PAT if METHOD == "pat" else compute_MAT)(attention, layer_norm=LAYER_NORM)
+    weights_per_layer = (compute_PAT if METHOD == "PAT" else compute_MAT)(attention, layer_norm=LAYER_NORM)
 
     # TODO Put the following outside the current loop, inside its own (anticipating intermediary writing of results to disk)
     # Take averages over groups of tokens
@@ -252,16 +252,17 @@ vmax = df_means.max().max()
 # Means for selected conditions
 
 # TODO Make this a global param; for plotting allow at most two factors? Compute diff only for 2 levels?
-factors_to_plot = ['anaphor_type', 'gender']
+factors_to_plot = ['anaphor_type']
 levels_horiz = items.levels[factors_to_plot[0]]
 levels_vert = items.levels[factors_to_plot[1]] if len(factors_to_plot) == 2 else [None]
+
+if len(levels_horiz) == 2:  # if two levels, also compute difference
+    levels_horiz.append('<DIFF>')
+if len(levels_vert) == 2:  # if two levels, also compute difference
+    levels_vert.append('<DIFF>')
+
 n_plots_horiz = len(levels_horiz)
 n_plots_vert = len(levels_vert)
-
-if n_plots_horiz == 2:  # if two levels, also compute difference
-    n_plots_horiz += 1
-if n_plots_vert == 2:  # if two levels, also compute difference
-    n_plots_vert += 1
 
 df_means = df_means.groupby(factors_to_plot + ['layer']).mean()
 
@@ -270,38 +271,57 @@ for l in range(n_layers):
 
     fig, axs = plt.subplots(ncols=n_plots_horiz, nrows=n_plots_vert, figsize=(4 * n_plots_horiz, 4 * n_plots_vert))
     plt.subplots_adjust(wspace=.6, top=.9)
-    fig.suptitle("Layer {}".format(l))
+    fig.suptitle("{}-scores given {} (layer {})".format(METHOD, ' × '.join(factors_to_plot), l))
+
+    # Keep for computing differences
+    weights_for_diff = [[None,None],
+                       [None,None]]
 
     for h, level_horiz in enumerate(levels_horiz):
+
         for v, level_vert in enumerate(levels_vert):
 
             index = items.groups if GROUPED else None # TODO Replace None by exemplary tokens... items.iloc[0].tokenized.split()
 
-            weights = df_means.loc[(level_horiz, level_vert, l)] if level_vert is not None else df_means.loc[(level_horiz, l)]
-            weights = weights.values
-            dim = int(np.sqrt(weights.shape[-1]))
-            weights = weights.reshape(dim, dim)
-            if TRANSPOSE:
-                weights = weights.transpose()
-            weights = pd.DataFrame(weights, index=index, columns=index)
+            is_difference_plot = True
+            if level_horiz != "<DIFF>" and level_vert == "<DIFF>":
+                weights = weights_for_diff[h][0] - weights_for_diff[h][1]
+            elif level_horiz == "<DIFF>" and level_vert != "<DIFF>":
+                weights = weights_for_diff[0][v] - weights_for_diff[1][v]
+            elif level_horiz == "<DIFF>" and level_vert == "<DIFF>":
+                weights = weights_for_diff[0][0] - weights_for_diff[1][1]
+            else:
+                is_difference_plot = False
+                weights = df_means.loc[(level_horiz, level_vert, l)] if level_vert is not None else df_means.loc[(level_horiz, l)]
+                weights = weights.values
+                dim = int(np.sqrt(weights.shape[-1]))
+                weights = weights.reshape(dim, dim)
+                if TRANSPOSE:
+                    weights = weights.transpose()
+                weights = pd.DataFrame(weights, index=index, columns=index)
+                # Keep for difference plot
+                weights_for_diff[h][v] = weights
 
-            ax = sns.heatmap(weights, xticklabels=True, yticklabels=True, vmin=vmin, vmax=vmax, linewidth=0.5, ax=axs[h,v] if level_vert is not None else axs[h], cbar=False, cmap="Blues", square=True, cbar_kws={'shrink':.5}, label='small')
-            ax.set_title('{} × {}'.format(level_horiz, level_vert) if level_vert is not None else level_horiz)
+            # TODO Consider setting global vmin/vmax only in case of MAT; in that case also for is_difference_plot.
+            ax = sns.heatmap(weights,
+                             xticklabels=True,
+                             yticklabels=True,
+                             vmin=vmin if not is_difference_plot else None,
+                             vmax=vmax if not is_difference_plot else None,
+                             center=0 if is_difference_plot else None,
+                             linewidth=0.5,
+                             ax=axs[h,v] if level_vert is not None else axs[h],
+                             cbar=False,
+                             cmap="coolwarm_r" if is_difference_plot else "Blues",
+                             square=True,
+                             cbar_kws={'shrink':.5},
+                             label='small')
+            if is_difference_plot:
+                ax.set_title('Difference')
+            else:
+                ax.set_title('{} × {}'.format(level_horiz, level_vert) if level_vert is not None else level_horiz)
             # ax.xaxis.tick_top()
             plt.setp(ax.get_yticklabels(), rotation=0)
-
-    # ## Difference plot
-    # # TODO Plot this only when factor has two levels
-    # diff = dfs_to_plot[0] - dfs_to_plot[1].values
-    # # TODO change values to be only shared tokens
-    # # TODO Compute this globally, too; or only if MAT?
-    # vmin2, vmax2 = diff.min().min(), diff.max().max()
-    # vmin2 = -(max(0-vmin2, vmax2))
-    # vmax2 = (max(0-vmin2, vmax2))
-    #
-    # ax = sns.heatmap(diff.transpose(), xticklabels=True, yticklabels=True, center=0, vmin=vmin2, cbar=False, linewidth=0.5, ax=axs[each_item_index + 1], cmap="coolwarm_r", square=True, cbar_kws={'shrink':.5}, label='small')
-    # ax.xaxis.tick_top()
-    # plt.setp(ax.get_xticklabels(), rotation=90)
 
     # TODO More meaningful output names
     out_filename = "{}/temp{}.png".format(out_path,l)
