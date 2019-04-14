e3ea50598a4973b407be27b2628bb17883f6451e
matthijswestera <matthijs.westera@gmail.com>
2019-04-14 22:07:45 +0200

    add random score to baseline

--- data_utils.py
+++ data_utils.py
@@ -161,6 +161,7 @@ def dependency_baseline(n):
 
     baseline_left_score = 0.0
     baseline_right_score = 0.0
+    baseline_random_score = 0.0
     gold_score = 0.0
     total = 0.0
 
@@ -183,13 +184,18 @@ def dependency_baseline(n):
             baseline_left_score += tree_utils.head_attachment_score(baseline_left, arcs)
             baseline_right_score += tree_utils.head_attachment_score(baseline_right, arcs)
             gold_score += tree_utils.head_attachment_score(arcs, arcs) # sanity check
+            # In mathematics, Cayley's formula is a result in graph theory named after Arthur Cayley. It states that for every positive integer n, the number of trees on n labeled vertices is n n − 2 {\displaystyle n^{n-2}} n^{n-2}.
+            baseline_random_score += 1.0 / (len(nodes) ** (len(nodes) - 2))
+
             total += 1.0
 
     baseline_left_score /= total
     baseline_right_score /= total
+    baseline_random_score /= total
+
     gold_score /= total
 
-    print("BASELINE LEFT:", baseline_left_score, "RIGHT:", baseline_right_score, "(gold: {})".format(gold_score))
+    print("BASELINE LEFT:", baseline_left_score, "RIGHT:", baseline_right_score, "RANDOM:", baseline_random_score, "(gold: {})".format(gold_score))
 
 
 def write_file_for_nominal_core_args():
