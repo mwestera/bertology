a5cafc4f62761c5fed2d781e8ccb4c8203fbb4de
matthijswestera <matthijs.westera@gmail.com>
2019-08-13 20:42:25 +0200

    simplify correlation

--- correlation.py
+++ correlation.py
@@ -26,13 +26,26 @@ def main():
 
     pearsons = []
     pvalues = []
-    for d1,d2 in zip(data1, data2):
-        pearson = tree_utils.pearson_correlation(d1, d2)
-        pearsons.append(pearson[0])
-        pvalues.append(pearson[1])
+    data1 = np.concatenate([d1.reshape(12,-1) for d1 in data1], axis=-1)
+    data2 = np.concatenate([d2.reshape(12, -1) for d2 in data2], axis=-1)
 
-    print(np.nanmean(pearsons), min(pearsons), max(pearsons), np.var(pearsons))
-    print(np.nanmean(pvalues), min(pvalues), max(pvalues), np.var(pvalues))
+    pearson = tree_utils.correlation(data1, data2)
+    print(pearson[0], pearson[1])
+
+    for n, (d1, d2) in enumerate(zip(data1, data2)):
+        pearson = tree_utils.correlation(d1, d2)
+        print(n, pearson[0], pearson[1])
+
+
+    # for d1,d2 in zip(data1, data2):
+    #     print(d1.reshape(12,-1).shape, d2.reshape(12,-1).shape)
+    #     quit()
+    #     pearson = tree_utils.pearson_correlation(d1, d2)
+    #     pearsons.append(pearson[0])
+    #     pvalues.append(pearson[1])
+
+    # print(np.nanmean(pearsons), min(pearsons), max(pearsons), np.var(pearsons))
+    # print(np.nanmean(pvalues), min(pvalues), max(pvalues), np.var(pvalues))
 
 
 if __name__ == "__main__":
